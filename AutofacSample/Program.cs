8eedb0a06e71aa11047546901a9bdca8a933a185
Aaron Burk <aburk762@cable.comcast.com>
2020-04-21 10:00:55 -0500

    first commit

---
+++ AutofacSample/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AutofacSample
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
